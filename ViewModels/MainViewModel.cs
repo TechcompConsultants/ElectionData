d0eaaf487ac7e38a25637f6c7ee82c7ddf213702
David W Dupes <46641607+TechcompConsultants@users.noreply.github.com>
2024-08-04 09:30:34 -0600

    Add totals and footer

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -4,11 +4,14 @@ using System.Collections.ObjectModel;
 using System.IO;
 using ElectionData.Models;
 using System.Globalization;
+using System.Linq; // Make sure to include this namespace
 
 namespace ElectionData.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        private const decimal RegisteredVoters = 21392464; // Constant for registered voters
+
         private ObservableCollection<Candidate> candidates;
         public ObservableCollection<Candidate> Candidates
         {
@@ -17,6 +20,30 @@ namespace ElectionData.ViewModels
             {
                 candidates = value;
                 OnPropertyChanged(nameof(Candidates));
+            }
+        }
+
+        private decimal totalVotes;
+        public decimal TotalVotes
+        {
+            get { return totalVotes; }
+            set
+            {
+                totalVotes = value;
+                OnPropertyChanged(nameof(TotalVotes));
+                // Update the TotalVotesPercentage whenever TotalVotes is set
+                TotalVotesPercentage = (TotalVotes / RegisteredVoters) * 100;
+            }
+        }
+
+        private decimal totalVotesPercentage;
+        public decimal TotalVotesPercentage
+        {
+            get { return totalVotesPercentage; }
+            set
+            {
+                totalVotesPercentage = value;
+                OnPropertyChanged(nameof(TotalVotesPercentage));
             }
         }
 
@@ -31,6 +58,7 @@ namespace ElectionData.ViewModels
             var json = File.ReadAllText(jsonFilePath);
             var candidatesList = JsonConvert.DeserializeObject<CandidatesWrapper>(json);
             var totalVotes = candidatesList.Candidates.Sum(c => c.Votes);
+            TotalVotes = totalVotes; // Update TotalVotes property
             foreach (var candidate in candidatesList.Candidates)
             {
                 // Update property type and calculation
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,12 +9,44 @@ namespace ElectionData.Views
         {
             InitializeComponent();
             DataContext = new MainViewModel();
+
+            // Subscribe to the PropertyChanged event to update footer visibility
         }
 
         private void ReloadData_Click(object sender, RoutedEventArgs e)
         {
             var viewModel = DataContext as MainViewModel;
             viewModel?.LoadData();  // Call LoadData again to reload the data
+        }
+
+        private void MainWindow_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue is MainViewModel oldViewModel)
+            {
+                oldViewModel.PropertyChanged -= MainWindow_PropertyChanged;
+            }
+
+            if (e.NewValue is MainViewModel newViewModel)
+            {
+                newViewModel.PropertyChanged += MainWindow_PropertyChanged;
+                UpdateFooterVisibility(newViewModel.TotalVotes);
+            }
+        }
+
+        private void MainWindow_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "TotalVotes")
+            {
+                var viewModel = DataContext as MainViewModel;
+                UpdateFooterVisibility(viewModel.TotalVotes);
+            }
+        }
+
+        private void UpdateFooterVisibility(decimal totalVotes)
+        {
+            bool isVisible = totalVotes == 10058774;
+            FooterTextBlock.Visibility = isVisible ? Visibility.Visible : Visibility.Collapsed;
+            FooterNoteTextBlock.Visibility = isVisible ? Visibility.Visible : Visibility.Collapsed;
         }
     }
 }
